fa45c42564c45114f891142b644edf79f9a133c5
roman <roman.lapacherie@gmail.com>
2021-11-11 17:25:41 +0000

    fix config

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,25 @@ import React from 'react';
 import styles from '../styles/Home.module.css'
 import {initializeApp} from 'firebase/app';
 import {doc, getFirestore, updateDoc} from 'firebase/firestore';
+import {getPerformance} from "firebase/performance";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_API_KEY,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASURE_ID
+  apiKey: "AIzaSyBlazGlIWRSB5SuBk_rP8mCofOPE_Dwms8",
+  authDomain: "five-stars-areeltrip.firebaseapp.com",
+  databaseURL: "https://five-stars-areeltrip-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "five-stars-areeltrip",
+  storageBucket: "five-stars-areeltrip.appspot.com",
+  messagingSenderId: "189009950416",
+  appId: "1:189009950416:web:304a18ade6c99c466910d5",
+  measurementId: "G-TH67C7W223"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
+if (process.browser) {
+  getPerformance(firebaseApp);
+}
 
 export default function Home({posts}) {
 
