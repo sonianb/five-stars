677b5ff9144f8a928644209783d2046669dbad43
sonianb <sonia.botsarova@gmail.com>
2022-01-19 18:08:02 +0000

    create and setup movie page

---
+++ pages/movie/id.tsx
@@ -0,0 +1,15 @@
+function moviePage() {
+    return <div>
+        <h1>Movie Title</h1>
+        <img src="" alt=""></img>
+        <section>Information about the film
+            <p>Plot description</p>
+            <p>Director</p>
+            <p>Writers</p>
+            <p>Stars</p>
+        <span>Rating</span>
+        </section>
+    </div>
+}
+
+export default moviePage;
