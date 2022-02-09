9306124166712650b97bb9b488260d44aebd56f8
sonianb <sonia.botsarova@gmail.com>
2022-02-09 21:25:23 +0000

    update footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css';
 import Link from 'next/link';
 
 export default function Footer() {
-    return <footer className={styles.footer}>Developed by <a href="https://github.com/sonianb">Sonia</a> &amp; <a href="https://github.com/romanlp">Roman</a>. 
+    return <footer className={styles.footer}>Developed by <a href="https://github.com/sonianb">Sonia</a> &amp; <a href="https://github.com/romanlp">Roman</a>.
 
     </footer>
 }
