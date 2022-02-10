89fb44aca20ff24519c941d19f080ccb4ed0a522
sonianb <sonia.botsarova@gmail.com>
2022-02-10 22:39:24 +0000

    remove container for movie details

--- pages/movie/[id].tsx
+++ pages/movie/[id].tsx
@@ -27,11 +27,9 @@ function MoviePage() {
             <div id={styles.imgGrid}>
                 <Image loader={tmdbLoader} src={data.poster_path} alt={data.title} width={200} height={300} />
             </div>
-            <div>
-                <p className={styles.listItem} id={styles.plotGrid}>Plot {data.overview}</p>
-                <p className={styles.listItem} id={styles.genreGrid}>Genre {data.genres.map(e => e.name).join(', ')}</p>
-                <p className={styles.listItem} id={styles.productionGrid}>Production Companies {data.production_companies.map(e => e.name).join(', ')}</p>
-            </div>
+            <p className={styles.listItem} id={styles.plotGrid}>Plot {data.overview}</p>
+            <p className={styles.listItem} id={styles.genreGrid}>Genre {data.genres.map(e => e.name).join(', ')}</p>
+            <p className={styles.listItem} id={styles.productionGrid}>Production Companies {data.production_companies.map(e => e.name).join(', ')}</p>
             <section className={styles.ratingContainer} id={styles.ratingGrid}>
                 <p>Rate the movie</p>
                 <Ratings movie={data} />
