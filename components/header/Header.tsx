f49e09aa38ff1face7bd0e6a3b05b5fbec998f15
romanlp <roman.lapacherie@gmail.com>
2022-03-03 19:02:02 +0000

    show user in header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,24 @@
-import styles from './Header.module.css';
+import Image from 'next/image';
 import Link from 'next/link';
 
+import styles from './Header.module.css';
+import { useAuth } from '../../lib/firebase';
+
 export default function Header() {
+    const { user, signOutUser } = useAuth();
+
     return <header className={styles.header}>Five Stars
         <nav className={styles.navitem}>
            <Link href="/">Home</Link>
+
+           { user ? 
+            <div>
+                <div>{ user.email }</div>
+                <img src={user.photoURL} width={40} height={40}></img>
+                <button onClick={() => signOutUser()}>Log out</button>
+            </div>
+           : <div></div>
+           }
         </nav>
     </header>
 }
